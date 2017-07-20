159a6e3529fb992e82adcfb51a444e81d2421ab1
Tilo Mitra <tilomitra@gmail.com>
2017-07-20 00:57:11 -0400

    update getstore policy

--- api/policies/getStore.js
+++ api/policies/getStore.js
@@ -1,13 +1,22 @@
 module.exports = function(req, res, next) {
     const Stores = sails.models.store;
+    var storeId = req.params.storeId;
     var nonce = req.headers.nonce;
 
-    Stores.findOne({ nonce: nonce })
-        .then(store => {
-            res.locals.store = store;
-            next();
-        })
-        .catch(err => {
-            return res.redirect(401);
-        });
+    if (storeId === req.session.store.id && nonce === req.session.store.nonce) {
+        res.locals.store = req.session.store;
+        next();
+    } else {
+        return res.redirect(401);
+    }
+
+    // Stores.findOne(storeId)
+    //     .then(store => {
+
+    //         res.locals.store = store;
+    //         next();
+    //     })
+    //     .catch(err => {
+    //         return res.redirect(401);
+    //     });
 };
