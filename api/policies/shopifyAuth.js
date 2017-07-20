86b63bf6d9407c1491deb05254ba4db1757b04a6
Tilo Mitra <tilomitra@gmail.com>
2017-07-20 01:29:23 -0400

    update policy

--- api/policies/shopifyAuth.js
+++ api/policies/shopifyAuth.js
@@ -1,4 +1,8 @@
 module.exports = (req, res, next) => {
-    if (req.session.store) return next();
-    else res.render("403");
+    if (req.session.store) {
+        sails.hooks.http.app.expose(req.session.store.id, "App.Store.id");
+        sails.hooks.http.app.expose(req.session.store.name, "App.Store.name");
+        sails.hooks.http.app.expose(req.session.store.nonce, "App.Store.nonce");
+        return next();
+    } else res.render("403");
 };
